71b557b0526817a2e762d0beb57ade62a56467cb
Arislanbek Kalbaev <127713555+arislanbek09@users.noreply.github.com>
2023-11-23 21:29:16 +0500

    Update task_1.h

--- src/task_1.h
+++ src/task_1.h
@@ -1,5 +1,39 @@
 /*
- * Author:
- * Date:
- * Name:
+ * Author: Arislanbek Kalbaev
+ * Date: November 23, 2023
+ * Name: Arislanbek Kalbaev
  */
+
+#include <iostream>
+using namespace std;
+
+void printNumbers(int start, int end) {
+
+    if (start == end) {
+        cout << start << " ";
+        return;
+    }
+
+    cout << start << " ";
+
+    if (start < end) {
+        printNumbers(start + 1, end);
+    } 
+    else {
+        printNumbers(start - 1, end);
+    }
+}
+
+int main() {
+    int i1, i2;
+    cin >> i1 >> i2;
+
+    if (A <= B) {
+        printNumbers(i1, i2);
+    } else {
+        printNumbers(i1, i2);
+    }
+
+    cout << endl;
+    return 0;
+}
